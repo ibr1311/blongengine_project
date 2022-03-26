bb804d4df9dd484e7e48e8beaf11c8b5889e47c5
 Macbook <macbook@Macbook-bema.local>
2022-03-26 21:05:06 +0600

    second

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,41 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import View
 # Create your views here.
 from .models import Post, Tag
+from .utils import *
+from .forms import TagForm, PostForm
 
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
+
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
 
 
